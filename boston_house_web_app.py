897e1aa958cc70d7d78d2c26709a97a1b3a28144
Somenath <somenathchoudhury35@gmail.com>
2021-11-11 18:05:02 +0530

    description

--- boston_house_web_app.py
+++ boston_house_web_app.py
@@ -55,6 +55,38 @@ st.write("### Boston Dataset")
 st.dataframe(X)
 st.write('---')
 
+# displaying the detaiils of input parameters
+st.write("### Details of Input Parameters")
+st.write("""
+**CRIM**:- Per capita crime rate by town.
+
+**ZN**:- Proportion of residential land zoned for lots over 25,000 sq. ft.
+
+**INDUS**:- Proportion of non-retail business acres per town.
+
+**CHAS**:- Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).
+
+**NOX**:- Nitric oxide concentration (parts per 10 million).
+
+**RM**:- Average number of rooms per dwelling.
+
+**AGE**:- Proportion of owner-occupied units built prior to 1940.
+
+**DIS**:- Weighted distances to five Boston employment centers.
+
+**RAD**:- Index of accessibility to radial highways.
+
+**TAX**:- Full-value property tax rate per $10,000.
+
+**PTRATIO**:- Pupil-teacher ratio by town.
+
+**B**:- 1000(Bk — 0.63)², where Bk is the proportion of [people of African American descent] by town.
+
+**LSTAT**:- Percentage of lower status of the population.
+
+""")
+st.write("---")
+
 
 df = user_input_features()
 
